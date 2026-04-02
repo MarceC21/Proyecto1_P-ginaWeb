bbce73d8974e64a505d67a3bfa5effaf5611ca68
MarceC21 <marcelap.castillo2102@gmail.com>
2026-04-01 23:01:27 -0600

    creación del componente PostCard para las cards de cada post

--- js/compartido/ui.js
+++ js/compartido/ui.js
@@ -1,2 +1,22 @@
 // Componentes compartidos, como botones o las cards de cada post
 // =========================    
+
+
+// Card de un post (para mostrar en la lista de posts)
+//Por el momento solo muestra el título, un extracto del body y el autor (userId)
+//FALTA AGREGAR EL BOTPN DE VER MÁS (QUE LLEVE AL DETALLE DEL POST)
+export function PostCard(post){
+        return `
+        <div class="PostCard">
+            <h3 class="card-title">${post.title}</h3>
+            
+            <p class="card-body">
+                ${post.body.substring(0, 100)}...
+            </p>
+            
+            <p class="card-author">
+                Autor: Usuario ${post.userId}
+            </p>
+        </div>
+    `;
+}
