d00aa348361e215f2f478cb5c87163a3162fd1ff
Sandoval <gtkaiser29@gmail.com>
2026-04-05 19:56:56 -0600

    feat: crear estructura base de seccion adicional favorito

---
+++ js/favoritos/favoritos.js
@@ -0,0 +1,9 @@
+const favoritesMessage = document.getElementById("favoritesMessage");
+const favoritesContainer = document.getElementById("favoritesContainer");
+
+function initFavoritesPage() {
+  favoritesMessage.textContent = "Sección de favoritos lista para implementar.";
+  favoritesContainer.innerHTML = "";
+}
+
+initFavoritesPage();
